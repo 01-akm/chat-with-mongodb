02c9490b58804a3d12152315de51a7a6a9df0a09
01-akm <kraadi4941@gmail.com>
2025-09-06 15:12:18 +0530

    We also need to add a piece of Express "middleware" that allows our server to understand incoming JSON data, which is how our frontend will send user registration details.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 // Load env vars
 dotenv.config();
@@ -10,7 +11,13 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware to accept JSON data
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
+
+// Mount routers
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, chatoos backend is running!');
