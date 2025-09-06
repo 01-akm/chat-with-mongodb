8f9cf46de1fe3de5ce91941319bf150edbf96b5e
01-akm <kraadi4941@gmail.com>
2025-09-06 14:31:17 +0530

    app.listen(PORT,...): This command starts the server and makes it listen for incoming connections on the specified port. It also prints a message to the console to let us know it's running.  

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const PORT = process.env.PORT |
 app.get('/', (req, res) => {
   res.send('Hello, chatoos backend is running!');
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
