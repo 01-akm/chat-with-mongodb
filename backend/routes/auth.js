e91623340f3bad14269e5d516ccf58ec69a3cd6a
01-akm <kraadi4941@gmail.com>
2025-09-06 15:18:23 +0530

    const { registerUser } = require('../controllers/authController');: We imported the registerUser function that we just created.
    router.post('/register', registerUser);: Instead of the simple res.send() message, we are now passing the registerUser function directly to the route. Express will automatically execute this function whenever a POST request is made to /api/auth/register.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser } = require('../controllers/authController');
 
-// @route   POST /register
+// @route   POST /api/auth/register
 // @desc    Register a new user
 // @access  Public
-router.post('/register', (req, res) => {
-  res.send('User registration route');
-});
+router.post('/register', registerUser);
 
 module.exports = router;
