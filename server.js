7e00930642a928082d3e3a5b9bac77d09b01855b
01-akm <kraadi4941@gmail.com>
2025-09-06 14:31:32 +0530

    compleated server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const app = express();
+
 const PORT = process.env.PORT |
 
 | 5000;
+
 app.get('/', (req, res) => {
   res.send('Hello, chatoos backend is running!');
 });
